a1db58461515a62f5ddf8d4ec6267ce095f17830
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-03-08 14:54:14 +0300

    refactor: rename variables

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-  let lol = [];
+  let arr = [];
   if (matrix == undefined) return []
   for (let i = 0; i < matrix.length; i++) {
     if (i % 2 !== 0) {
-      lol = lol.concat(matrix[i].reverse());
+      arr = arr.concat(matrix[i].reverse());
     } else {
-      lol = lol.concat(matrix[i]);
+      arr = arr.concat(matrix[i]);
     }
 
   }
-  return lol
+  return arr
 }
